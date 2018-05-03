5c7e3d4398ad7e66d7b14de7dc25fe19f559e688
Eimar <eimar.urbina@gmail.com>
2018-05-03 10:43:21 -0400

    Añadiendo migración para respuestas

---
+++ database/migrations/2018_04_30_221112_create_respuestas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRespuestasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respuestas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->boolean('habilitada')->default(0);
+            
+            $table->integer('pregunta_id')->unsigned();
+            $table->foreign('pregunta_id')->references('id')->on('preguntas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('respuestas');
+    }
+}
